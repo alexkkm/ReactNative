c9be30b30db575def3a52b0a369713a659d80bb8
alexkkm <alexkong0222@gmail.com>
2022-04-02 21:46:28 +0800

    update state.js with useEffect()

--- screens/Basic/State.js
+++ screens/Basic/State.js
@@ -27,34 +27,31 @@ const StateScreen = () => {
     }
 
     return (
+        <Clock time={time}/>
+    );
+};
+
+const Clock=(props)=>{
+    return(
         <ScrollView>
         <View>
         
-            <Text>Current Time is: {time}</Text>
+            <Text style={styles.paragraph}>Current Time is: {props.time}</Text>
         
         </View>
         </ScrollView>
     );
 };
 
-const Clock=(props)=>{
-    return(
-        null
-    );
-};
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
-      padding: 8,
-    },
     paragraph: {
       margin: 24,
       fontSize: 18,
       fontWeight: 'bold',
       textAlign: 'center',
+      borderWidth: 3,
+      borderRadius: 5,
+      borderColor: 'grey'
     },
   });
 
